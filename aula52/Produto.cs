59ea79660e78d143c8f67fdd68a31946bbcd49e2
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-07-06 23:52:30 -0300

    Aula 52 finalizada

---
+++ aula52/Produto.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco; 
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5; 
+        }
+
+        public Produto()
+        {
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+        + ", R$"
+        + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", "
+        + Quantidade
+        + " unidades, Total: R$"
+        + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
