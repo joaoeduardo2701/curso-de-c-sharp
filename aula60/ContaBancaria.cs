451313dcc3bf8f97808ebc75912c7f6b737898ef
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-07-12 16:53:57 -0300

    Exercícios da aula 60 finalizados

---
+++ aula60/ContaBancaria.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace aula60
+{
+    class ContaBancaria
+    {
+        public int NumeroDaConta { get; private set; }
+        public string TitularDaConta { get; private set; }
+        public double Saldo { get ; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            NumeroDaConta = numero;
+            TitularDaConta = titular;
+        }
+
+        public double Deposito(double valorDeposito)
+        {
+            return Saldo += valorDeposito; 
+        }
+
+        public void Saque(double valorSaque)
+        {
+            Saldo -= valorSaque;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + NumeroDaConta + ", Titular: " + TitularDaConta + ", Saldo: " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+ 
---
+++ aula60/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace aula60
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial? ");
+            char depositoInicial = char.Parse(Console.ReadLine());
+            
+            ContaBancaria cb = new ContaBancaria(numero, titular);
+            double valorDeposito = 0.0;
+
+            if (depositoInicial == 's')
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                 valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                 cb.Deposito(valorDeposito);
+            }
+            else
+            {
+                cb.Deposito(valorDeposito);
+            }
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(cb);
+
+            Console.Write("Entre com um valor para depósito: ");
+            valorDeposito = double.Parse(Console.ReadLine());
+            cb.Deposito(valorDeposito);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cb);
+
+            Console.Write("Digite um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cb.Saque(valorSaque);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cb);
+        }
+    }
+}
