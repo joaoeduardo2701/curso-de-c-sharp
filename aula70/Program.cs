8d4658b2a8f0b2b2e6f0c10d35e9225c5b5f7b8b
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-15 14:32:13 -0300

    Aula 70 finalizada

--- aula70/Program.cs
+++ aula70/Program.cs
@@ -1,28 +1,32 @@
 ﻿using System;
+using System.Globalization;
 
 namespace aula70
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite um número: ");
+            Console.Write("Digite a quantidade de produtos: ");
             int n1 = int.Parse(Console.ReadLine());
 
-            int[] numeros = new int[n1];
+            double[] precos = new double[n1];
+
+            double somaPreco = 0.0;
 
             for (int i = 0; i < n1; i++)
             {
-                Console.Write($"Digite o {i}º número: ");
-                numeros[i] = int.Parse(Console.ReadLine());
-            }
-            /*
-            for (int i = 0; i <= n1; i++)
-            {
-                Console.WriteLine(numeros[i]);
-            }
-            */
-            Console.WriteLine(numeros[1]);
+                Console.Write($"Digite o nome do {i + 1}º produto: ");
+                Console.ReadLine();
+
+                Console.Write($"Digite o preço do produto: ", CultureInfo.InvariantCulture);
+                precos[i] = double.Parse(Console.ReadLine());
+
+                somaPreco += precos[i];
+            } 
+
+            double media = somaPreco / n1;
+            Console.WriteLine("Média: " + media.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
