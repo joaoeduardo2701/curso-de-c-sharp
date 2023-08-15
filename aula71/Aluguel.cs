792d3f9451d243b96f4263078abb03c12b8c5146
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-15 15:14:23 -0300

    Aula 71 finalizada

---
+++ aula71/Aluguel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace aula71
+{
+    class Aluguel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set;}
+        public int Quarto { get; set;}
+
+        public override string ToString()
+        {
+            return $"{Quarto}: {Nome}, {Email}";
+        }
+    }
+}
---
+++ aula71/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace aula71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int quantidadeQuartos = int.Parse(Console.ReadLine());
+
+            Aluguel[] a = new Aluguel[10];
+
+            for (int i = 0; i < quantidadeQuartos; i++)
+            {
+                Console.WriteLine($"Aluguel #{i + 1}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Número do quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                a[i] = new Aluguel { Nome = nome, Email = email, Quarto = quarto};
+            }
+
+            for (int i = 0; i < quantidadeQuartos; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            // Console.WriteLine("Nome: " + a[0].Nome);
+        }
+    }
+}
