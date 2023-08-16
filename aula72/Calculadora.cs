c09702f7c424eb5b3011e5cf9d82bae6f6d60c96
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-16 10:05:54 -0300

    Aula 72 finalizada

---
+++ aula72/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula72
+{
+    class Calculadora
+    {
+        public static int Soma(params int[] numeros)
+        {
+            int soma = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                soma += numeros[i];
+            }
+            return soma;
+        }
+    }
+}
---
+++ aula72/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int s1 = Calculadora.Soma(new int[] {5, 7});
+            Console.WriteLine(s1);
+            */
+
+            int s1 = Calculadora.Soma(5, 7);
+            int s2 = Calculadora.Soma(10, 17, 15);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+        }
+    }
+}
