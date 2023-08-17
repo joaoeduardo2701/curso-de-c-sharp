922a7c67a5fe1d102b99579dff73585d3298ceca
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-17 17:53:06 -0300

    Aula 75 finalizada

---
+++ aula75/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = { "João", "Maria", "Leonardo" };
+
+            for (int n = 0; n < nomes.Length; n++)
+            {
+                Console.WriteLine(nomes[n]);
+            }
+
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine($"{nome} sa primeira letra do seu nome é {nome[0]}");
+            }
+        }
+    }
+}
