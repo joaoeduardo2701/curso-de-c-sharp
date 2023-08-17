3e9b08482af778f99561eb333e0092fb891d0c09
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-17 15:30:45 -0300

    Aula 73 finalizada

---
+++ aula73/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace aula73
+{
+    class Calculator
+    {
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+        public static void Triple(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ aula73/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int n = 5;
+            Calculator.Triple(ref n);
+            Console.WriteLine($"O triplo do número é {n}");
+            */
+
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+            Object obj = a;
+            Console.WriteLine(obj);
+
+        }
+    }
+}
