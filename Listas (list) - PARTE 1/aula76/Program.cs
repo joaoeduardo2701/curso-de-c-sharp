a4926a3784922ca042c52996e37c9b603549cf27
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-18 01:04:28 -0300

    Aula 76 finalizada

---
+++ Listas (list) - PARTE 1/aula76/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aula76
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista1 = new List<string>();
+
+            List<string> lista2 = new List<string> { "Maria", "João", "Cleber" };
+
+            /* 
+               ** Vantagens da lista **
+               
+               1- O tamanho da lista é variável 
+               2- Facilidade para se realizar inserções e deleções
+                
+               ** Desvantagens da lista **
+               
+               1- Acesso sequencial aos elementos
+             */
+        }
+    }
+}
