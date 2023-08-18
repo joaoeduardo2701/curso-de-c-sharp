982d31965cd6f8aba10f29897757ff55fe19d325
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-18 14:53:52 -0300

    Inicio do exercício

---
+++ Exercício de fixação (listas)/aula78/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aula78
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; } = 1000.00;
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario += Salario * (porcentagem / 100);
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", " + Salario;
+        }
+    }
+}
--- Exercício de fixação (listas)/aula78/Program.cs
+++ Exercício de fixação (listas)/aula78/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace aula78
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Quantos funcionário vão ser registrados? ");
+            int quantidadeFuncionarios = int.Parse(Console.ReadLine());
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            for (int i = 1; i <= quantidadeFuncionarios; i++)
+            {
+                Console.WriteLine("Funcionário #" + i);
+
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine());
+
+                funcionarios.Add(new Funcionario(id, nome, salario));
+            }
+
+            Console.WriteLine("Lista de funcionarios: ");
+            foreach (var funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario);
+            }
+
+            Console.Write("Digite o id do funcionário que deseja dar um aumento: ");
+            int idAumento = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a porcentagem de aumento: ");
+            double porcentagem = double.Parse(Console.ReadLine());
+
         }
     }
 }
