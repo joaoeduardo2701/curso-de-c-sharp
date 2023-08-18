ad4b95dc21958aa5b3748109b37991846c0fb077
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-18 14:12:17 -0300

    Aula 77 finalizada

--- Listas (list) - PARTE 2/aula77/Program.cs
+++ Listas (list) - PARTE 2/aula77/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace aula77
 {
@@ -6,7 +7,78 @@ namespace aula77
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> list = new List<string>();
+
+            // Adicionando elementos na lista
+
+            list.Add("Pedro");
+            list.Add("Cleber");
+
+            list.Insert(0, "Rodrigo");
+            list.Insert(1, "César");
+
+            foreach (string nome in list)
+            {
+                Console.WriteLine($"Olá, {nome} é um prazer te conhecer");
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("List count: " +  list.Count);
+            Console.WriteLine(" ");
+
+            // Encontrando elementos
+
+            string s1 = list.Find(Test);
+            Console.WriteLine("Primeira pessoa com a letra C: " + s1);
+
+            string s2 = list.FindLast(Test);
+            Console.WriteLine("ùltima pessoa com a letra: " + s2);
+
+            int pos1 = list.FindIndex(Test);
+            Console.WriteLine("Posição da primeira pessoa que começa com a letra C: " + pos1);
+
+            int pos2 = list.FindLastIndex(Test);
+            Console.WriteLine("Posição da primeira pessoa que começa com a letra C: " + pos2);
+
+            Console.WriteLine(" ");
+
+            // Filtrando a lista
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("Nomes que tem 5 letras: ");
+            foreach (string nome in list2)
+            {
+                Console.WriteLine(nome);
+            }
+
+            Console.WriteLine(" ");
+
+            // Removendo itens
+
+            list.Remove("Cleber");
+
+            foreach (string nome in list)
+            {
+                Console.Write(nome + " ");
+            }
+
+            list.RemoveAll(x => x[0] == 'R');
+
+            Console.WriteLine("\n-----------------");
+
+            foreach (string nome in list)
+            {
+                Console.Write(nome + " ");
+            }
+
+            Console.WriteLine("\n-----------------");
+
+            list.RemoveAt(0);
+        }
+
+        static bool Test(string s)
+        {
+            return s[0] == 'C';
         }
     }
 }
