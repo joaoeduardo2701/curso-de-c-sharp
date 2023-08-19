1b4aa21c91d6b15523e0938e44ece1980263dedb
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-18 22:05:22 -0300

    Aula 79 finalizada

---
+++ Matrizes/aula79/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aula79
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matriz = new double[2, 3];
+
+            Console.WriteLine(matriz.Length); // Quantiadade de espaços na matriz
+             
+            Console.WriteLine(matriz.Rank); // Mostra a primeira dimenção da matriz, ou seja, as linhas
+
+            Console.WriteLine(matriz.GetLength(0)); // Mostra a quantidade de linhas da matriz
+            Console.WriteLine(matriz.GetLength(1)); // Mostra a quantidade de colunas da matriz
+            Console.WriteLine(matriz.GetLength(2));
+        }
+    }
+}
+ 
