39844b1d5c0dbabcb30eeda4707b0389d7ab7f88
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-08-19 23:57:02 -0300

    Aula 89 finalizada

---
+++ DateTime - representando data e hora/aula89/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace aula89
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            DateTime d1 = DateTime.Now;
+            DateTime d2 = DateTime.UtcNow;
+            DateTime d3 = DateTime.Today;
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            */
+
+            /*
+            DateTime d1 = new DateTime(2000, 8, 15);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58, 275);
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            */
+
+            /*
+            DateTime d1 = DateTime.Parse("2000-08-15");
+            DateTime d2 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d3 = DateTime.Parse("15/08/2000");
+            DateTime d4 = DateTime.Parse("15/08/2000 13:05:58");
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            */
+
+            DateTime d1 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime d2 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss",CultureInfo.InvariantCulture);
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+
+        }
+    }
+}
