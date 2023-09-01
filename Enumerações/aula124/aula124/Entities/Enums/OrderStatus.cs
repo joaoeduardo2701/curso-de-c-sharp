5e10ee029e8963de48c276cba1d176d9695d34d4
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-09-01 16:11:19 -0300

    Aula 124 finalizada

---
+++ Enumerações/aula124/aula124/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace aula124.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendigPayment = 0, 
+        Processing = 1, 
+        Shipped = 2,
+        Delivered = 3
+    }
+}
--- Enumerações/aula124/aula124/Program.cs
+++ Enumerações/aula124/aula124/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
+using aula124.Entities;
+using aula124.Entities.Enums;
+
 namespace aula124
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendigPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.Delivered.ToString();
+
+            Console.WriteLine(txt);
         }
     }
 }
