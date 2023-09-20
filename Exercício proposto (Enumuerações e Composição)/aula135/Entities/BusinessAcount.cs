f13480014e93ffc9c5045193d8d69179d5ae111a
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2023-09-19 23:55:01 -0300

    Aula 135 finalizada

---
+++ Exercício proposto (Enumuerações e Composição)/aula135/Entities/BusinessAcount.cs
@@ -0,0 +1,22 @@
+﻿namespace aula135.Entities
+{
+    class BusinessAcount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAcount() { }
+
+        public BusinessAcount(string holder, double balance, int number, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
